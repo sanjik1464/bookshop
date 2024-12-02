f7f62675f4ba773a9ae77980d85ff8d9a9bd792c
sanjik1464 <sanjaralikuychibaev@gmail.com>
2024-12-02 17:05:38 +0500

    first

---
+++ main/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+# from django.views.decorators.cache import cache_page
+
+from main import views
+
+app_name = 'main'
+
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('about/', views.AboutView.as_view(), name='about'),
+]
